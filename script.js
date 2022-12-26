27d89fd5f490f90ca60efaecfd92576f7d66be4e
heclop <hlopez2510@gmail.com>
2022-12-26 13:08:01 +0000

    Got squares to be added into grid with loop

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const btnStart = document.querySelector('#btn-start');
+btnStart.addEventListener(`click`, promptUser);
+const grid = document.querySelector('.grid');
 
-btnStart.addEventListener('click', promptUser);
 
 function promptUser() {
-    let gridSize = prompt(`Pick grid size between 16 and 100`);
-    if (gridSize < 16 || gridSize >100) {
+    let gridSize = parseInt(prompt(`Pick grid size between 16 and 100`));
+    if (gridSize < 16 || gridSize > 100) {
         alert(`Please pick a number between 16 and 100`);
     } else {
         createGrid(gridSize);
@@ -12,10 +13,13 @@ function promptUser() {
 }
 
 function createGrid(gridSize) {
-    const grid = document.querySelector('.grid');
-    const div = document.createElement('div');
+    
     for (let i=0; i < gridSize; i++) {
-        div.classList.add('grid-item');
-        document.getElementById('grid').appendChild(div);
+        const square = document.createElement('div');
+        square.classList.add('grid-item');
+        document.querySelector('.grid').appendChild(square);
+        console.log(`test`);
     }
 }
+
+
