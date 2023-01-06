4a2267548c1621b2bb0abd5f3d9fa2b5c6666ee3
heclop <hlopez2510@gmail.com>
2023-01-06 16:11:55 +0000

    fixed start game function to delete previous grid then and add new grid

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const btnStart = document.querySelector('#btn-start');
 btnStart.addEventListener(`click`, promptUser);
 const grid = document.querySelector('.grid');
+const squares = document.querySelectorAll('.grid-item');
+
 
 
 function promptUser() {
+    document.querySelector('.grid').innerHTML = '';
     let gridSize = parseInt(prompt(`Pick grid size between 16 and 100`));
     if (gridSize < 16 || gridSize > 100) {
         alert(`Please pick a number between 16 and 100`);
     } else {
         createGrid(gridSize);
+        const squares = document.querySelectorAll('.grid-item');
+        squares.forEach((square) => {
+        square.addEventListener(`mouseover`, changeColor);
+        } );
     }
 }
 
@@ -18,8 +25,12 @@ function createGrid(gridSize) {
         const square = document.createElement('div');
         square.classList.add('grid-item');
         document.querySelector('.grid').appendChild(square);
-        console.log(`test`);
+        
     }
 }
-
-
+           
+function changeColor(e) {
+        e.target.style.backgroundColor = `black`;
+    
+    
+}
